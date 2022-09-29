843525b189cc538adca01c8991354ba04bca4536
David Cuccia <dcuccia@gmail.com>
2022-09-29 16:31:21 -0700

    Update with impl showing how to work-around using generic arguments and to side-step runtime issues with NSubstitute mocks.

--- BusinessApiTests.cs
+++ BusinessApiTests.cs
@@ -4,14 +4,24 @@ using Xunit;
 
 public class BusinessApiTests
 {
+    public class FakeCustomerService : ICustomerService
+    {
+        public virtual string ServiceName => "";
+        public virtual List<Customer> GetAllCustomers() => new();
+
+        public static void AddServiceDependencies(IServiceCollection services, IConfiguration configuration) { }
+    }
+
     [Fact]
     public static void GetPrintableCustomerList_ReturnsEmptyList_WhenNoCustomersExist()
     {
         // Arrange
-        
         // compiler error CS8920 "static member does not have a most specific implementation of the interface"
-        ICustomerService subCustomerService = NSubstitute.Substitute.For<ICustomerService>();
-        
+        // ICustomerService subCustomerService = NSubstitute.Substitute.For<ICustomerService>();
+        // NSubstitute (sometimes?) generates a runtime System.TypeLoadException : Virtual static method 'AddServiceDependencies' is not implemented on type 'Castle.Proxies.ObjectProxy_1' from assembly 'DynamicProxyGenAssembly2, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null'.
+        // ICustomerService subCustomerService = (ICustomerService)NSubstitute.Substitute.For(new[] { typeof(ICustomerService) }, new object[] { });
+        // To get this class to work reliably, mock a "fake" class with virtual methods for now, using it's interface:
+        ICustomerService subCustomerService = Substitute.For<FakeCustomerService>();
         subCustomerService.GetAllCustomers().Returns(new List<Customer>()); // MOCK EXAMPLE
         BusinessApi api = new (subCustomerService); 
         
@@ -29,12 +39,16 @@ public class BusinessApiTests
         WebApplicationBuilder builder = WebApplication.CreateBuilder();
         builder.Services.AddServicesForAssembly(builder.Configuration);
         // compiler error CS8920 "static member does not have a most specific implementation of the interface"
-        builder.Services.AddSingleton<ICustomerService>(services =>
+        //builder.Services.AddSingleton<ICustomerService>(services =>
+        //    CustomerServiceFactory.GetCustomerService(services, CustomerServiceType.CosmosDb));
+        builder.Services.AddSingleton(typeof(ICustomerService), services =>
             CustomerServiceFactory.GetCustomerService(services, CustomerServiceType.CosmosDb));
 
         // Act
         WebApplication app = builder.Build();
-        ICustomerService customerService = app.Services.GetRequiredService<ICustomerService>();
+        // compiler error CS8920 "static member does not have a most specific implementation of the interface"
+        // ICustomerService customerService = app.Services.GetRequiredService<ICustomerService>();
+        ICustomerService customerService = (ICustomerService)app.Services.GetRequiredService(typeof(ICustomerService));
 
         // Assert
         customerService.Should().NotBeNull();
--- Program.cs
+++ Program.cs
@@ -2,9 +2,13 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder();
 builder.Services.AddServicesForAssembly(builder.Configuration);
 // compiler error CS8920 "static member does not have a most specific implementation of the interface"
-builder.Services.AddSingleton<ICustomerService>(services =>
+//builder.Services.AddSingleton<ICustomerService>(services =>
+//    CustomerServiceFactory.GetCustomerService(services, CustomerServiceType.CosmosDb));
+builder.Services.AddSingleton(typeof(ICustomerService), services =>
     CustomerServiceFactory.GetCustomerService(services, CustomerServiceType.CosmosDb));
 
 WebApplication app = builder.Build();
 
-ICustomerService customerService = app.Services.GetRequiredService<ICustomerService>();
+// compiler error CS8920 "static member does not have a most specific implementation of the interface"
+//ICustomerService customerService = app.Services.GetRequiredService<ICustomerService>();
+ICustomerService customerService = (ICustomerService)app.Services.GetRequiredService(typeof(ICustomerService));
